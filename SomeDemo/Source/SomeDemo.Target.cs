a09fd9b7f004fa51e82f965cae121bf72bf6f6fd
Mateusz Chłopek <matikc25@gmail.com>
2023-07-30 11:46:46 +0200

    Changed UE version to 5.0.3
    Another project Init

--- SomeDemo/Source/SomeDemo.Target.cs
+++ SomeDemo/Source/SomeDemo.Target.cs
@@ -9,7 +9,6 @@ public class SomeDemoTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
-		ExtraModuleNames.Add("SomeDemo");
+		ExtraModuleNames.AddRange( new string[] { "SomeDemo" } );
 	}
 }
--- SomeDemo/Source/SomeDemoEditor.Target.cs
+++ SomeDemo/Source/SomeDemoEditor.Target.cs
@@ -9,7 +9,6 @@ public class SomeDemoEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
-		ExtraModuleNames.Add("SomeDemo");
+		ExtraModuleNames.AddRange( new string[] { "SomeDemo" } );
 	}
 }
